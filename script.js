3aa67442689e5921431215084ac55c900f9ba8a4
whosvoltex25-gif <whosvoltex25@gmail.com>
2026-08-01 10:57:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const submitPassword = document.getElementById('submitPassword');
 
     submitPassword.addEventListener('click', function() {
-        if (passwordInput.value.toLowerCase() === 'babycakes') {
+        if (passwordInput.value.toLowerCase() === '0725') {
             passwordModal.style.opacity = '0';
             setTimeout(() => {
                 passwordModal.style.display = 'none';
